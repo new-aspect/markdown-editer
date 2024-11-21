edec8f0db6887770cb9588205e66430600a5a6ed
nzhao <nzhao@sudytech.com>
2024-11-21 14:34:19 +0800

    定义了HTML格式

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {/* 左边是 textares */}
+      <div className='editor'>
+        <textarea placeholder='在这里输入 Markdown 内容'></textarea>
+      </div>
+
+      {/* 右边是渲染部分 */}
+      <div className='preview'>
+        {/* 暂时是空的 */}
+        <div>暂时是空的</div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
