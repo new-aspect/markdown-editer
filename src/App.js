9e20c8194479ffd5e7b49f05588a2b4a835cd2b9
nzhao <nzhao@sudytech.com>
2024-11-22 16:20:21 +0800

    实现实时预览功能

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // 使用content和setContent
+  // 那么这时候监听testarea的文字变化，然后将变化塞到setContent里面
+  const [content, setContent] = useState("")
+
+
   return (
     <div className='container'>
       {/* 左边是 textares */}
       <div className='editor'>
-        <textarea placeholder='在这里输入 Markdown 内容'></textarea>
+        <textarea 
+          placeholder='在这里输入 Markdown 内容' 
+          onChange={e=>{setContent(e.target.value)}}
+        ></textarea>
       </div>
 
       {/* 右边是渲染部分 */}
       <div className='preview'>
         {/* 暂时是空的 */}
-        <div>暂时是空的</div>
+        <div>{content}</div>
       </div>
     </div>
   )
